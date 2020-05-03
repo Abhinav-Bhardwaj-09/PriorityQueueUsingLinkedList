41a5b7933cef5e9f7b4ec6f42f4033d78240f4ad
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-05-03 12:20:18 +0530

    After Creating Node Class

--- src/Nodes/Node.java
+++ src/Nodes/Node.java
@@ -8,4 +8,48 @@ package Nodes;
  */
 
 public class Node {
+    private String Name;
+    private int Roll ;
+    private int CPI;
+    private Node next;
+
+    public Node(String name, int roll, int CPI, Node next) {
+        Name = name;
+        Roll = roll;
+        this.CPI = CPI;
+        this.next = null;
+    }
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getRoll() {
+        return Roll;
+    }
+
+    public void setRoll(int roll) {
+        Roll = roll;
+    }
+
+    public int getCPI() {
+        return CPI;
+    }
+
+    public void setCPI(int CPI) {
+        this.CPI = CPI;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
