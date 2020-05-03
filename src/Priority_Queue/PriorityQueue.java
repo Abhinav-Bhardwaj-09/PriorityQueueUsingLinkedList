9b1ef9f60c1db095f3e3121ad0de221dd5c0419c
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-05-03 12:36:20 +0530

    After creating Priority Queue Class

--- src/Priority_Queue/PriorityQueue.java
+++ src/Priority_Queue/PriorityQueue.java
@@ -1,5 +1,7 @@
 package Priority_Queue;
 
+import Nodes.Node;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Abhinav Bhardwaj
@@ -8,4 +10,52 @@ package Priority_Queue;
  */
 
 public class PriorityQueue {
+    Node front ;
+    Node rear ;
+
+    public PriorityQueue() {
+        this.front = null;
+        this.rear = null;
+    }
+
+
+    public void enqueue (Node newnode){
+        if ( front == null || newnode.getCPI() < front.getCPI() ){
+            newnode.setNext(front);
+            front = newnode;
+        }
+        else{
+            Node temp = front ;
+            while ( temp.getNext() != null && temp.getNext().getCPI() <= newnode.getCPI()) {
+                temp = temp.getNext();
+            }
+            newnode.setNext(temp.getNext());
+            temp.setNext(newnode);
+        }
+    }
+
+    public  void dequeue(){
+        if ( front != null){
+            front = front.getNext();
+        }
+        else {
+            System.out.println("Underflow... ");
+        }
+        if ( front != null ){
+            display();
+        }
+        else {
+            System.out.println("List is ");
+        }
+    }
+
+
+    public void display(){
+        Node temp = front;
+        while ( temp!= null ){
+            System.out.print(temp.getRoll() + ". ");
+            System.out.println(temp.getName());
+            temp = temp.getNext();
+        }
+    }
 }
